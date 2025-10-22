a41483a49aee0004c3845848a01b972b2091065e
TheAwesomeAJ <theawesomeaj.dev@gmail.com>
2025-10-22 09:30:30 -0400

    Update application command registration to use hardcoded client ID

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,7 +22,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 try {
   console.log("Started refreshing application (/) commands.");
   await rest.put(
-    Routes.applicationCommands(process.env.CLIENT_ID),
+    Routes.applicationCommands("1430260416709132431"),
     { body: commands },
   );
   console.log("Successfully reloaded application (/) commands.");
